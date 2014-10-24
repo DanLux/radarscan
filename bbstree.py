757d6b962028fcca5c9755b58428a280ed1cd613
Daniel Alves <danpaulalves@gmail.com>
2014-10-24 04:45:56 +0200

    Scan line radar algorithm. Need some fixes.

---
+++ bbstree.py
@@ -0,0 +1,284 @@
+# Generic BBST Node structure
+class Node:
+    def __init__(self, value, relation_function):
+        self.value = value
+        self.left_son = None
+        self.right_son = None
+        self.parent = None
+        self.height = 1
+        self.total_order_relation = relation_function
+
+    def is_right_child(self):
+        return self.parent.right_son == self
+
+    def is_left_child(self):
+        return self.parent.left_son == self
+
+    def update_height(self):
+        left_height = 0 if self.left_son is None else self.left_son.height
+        right_height = 0 if self.right_son is None else self.right_son.height
+        self.height = 1 + max(left_height, right_height)
+
+    def get_balance(self):
+        left_height = 0 if self.left_son is None else self.left_son.height
+        right_height = 0 if self.right_son is None else self.right_son.height
+        return left_height - right_height
+
+    def lesser_than(self, node):
+        return self.total_order_relation(self, node) == -1
+
+    def greater_than(self, node):
+        return self.total_order_relation(self, node) == 1
+
+    def equal(self, node):
+        return not(self.greater_than(node) or self.lesser_than(node))
+
+# Generic BBST structure
+class BalancedBinarySearchTree:
+    def __init__(self, function):
+        self.root = None
+        self.comparison_function = function
+
+    def contains(self, value):
+        node = self.__get(self.__create_node(value), self.root)
+        if node is None: return False
+        else: return True
+
+    def __create_node(self, value):
+        return Node(value, self.comparison_function)
+
+    def __get(self, node, root):
+        if root is None: return None
+
+        if node.lesser_than(root):
+            return self.__get(node, root.left_son)
+        elif node.greater_than(root):
+            return self.__get(node, root.right_son)
+        else:
+            return root
+
+    def insert(self, value):
+        new_node = self.__create_node(value)
+        if self.root is None:
+            self.root = new_node
+            self.root.parent = self.root
+        else: self.__insert(self.root, new_node)
+
+    def remove(self, value):
+        node = self.__get(self.__create_node(value), self.root)
+        if not(node.left_son is None or node.right_son is None):
+            successor = BalancedBinarySearchTree.min(node.right_son)
+            node.value = successor.value
+            node = successor
+
+        if node.left_son and (not node.right_son):
+            if self.root == node:
+                node.left_son.parent = node.left_son
+                self.root = node.left_son
+            else:
+                node.left_son.parent = node.parent
+                if node.is_left_child():
+                    node.parent.left_son = node.left_son
+                else:
+                    node.parent.right_son = node.left_son
+                self.__balance_tree_after_deletion(node.parent)
+
+        elif node.right_son and (not node.left_son):
+            if self.root == node:
+                node.right_son.parent = node.right_son
+                self.root = node.right_son
+            else:
+                node.right_son.parent = node.parent
+                if node.is_right_child():
+                    node.parent.right_son = node.right_son
+                else:
+                    node.parent.left_son = node.right_son
+                self.__balance_tree_after_deletion(node.parent)
+
+        else:
+            if self.root == node:
+                self.root = None
+            else:
+                if node.is_left_child():
+                    node.parent.left_son = None
+                else:
+                    node.parent.right_son = None
+                self.__balance_tree_after_deletion(node.parent)
+
+    @staticmethod
+    def min(root):
+        if root is None or root.left_son is None:
+            return root
+        else:
+            return BalancedBinarySearchTree.min(root.left_son)
+
+    def __insert(self, node, new_node):
+        if new_node.lesser_than(node):
+            if node.left_son:
+                self.__insert(node.left_son, new_node)
+            else:
+                node.left_son = new_node
+                node.left_son.parent = node
+                self.__balance_tree_after_insertion(node)
+        else:
+            if node.right_son:
+                self.__insert(node.right_son, new_node)
+            else:
+                node.right_son = new_node
+                node.right_son.parent = node
+                self.__balance_tree_after_insertion(node)
+
+    def __balance_tree_after_insertion(self, node):
+        node.update_height()
+        if node.get_balance() == 1 or node.get_balance() == -1:
+            if self.root != node:
+                self.__balance_tree_after_insertion(node.parent)
+        elif node.get_balance() == 2:
+            if node.left_son.get_balance() == 1:
+                self.__rotate_right(node)
+            else: self.__rotate_left_right(node)
+        elif node.get_balance() == -2:
+            if node.right_son.get_balance() == -1:
+                self.__rotate_left(node)
+            else: self.__rotate_right_left(node)
+
+    def __balance_tree_after_deletion(self, node):
+        node.update_height()
+        parent = node.parent
+
+        if node.get_balance() == 2:
+            if node.left_son.get_balance() == -1:
+                self.__rotate_left_right(node)
+            else: self.__rotate_right(node)
+        if node.get_balance() == -2:
+            if node.right_son.get_balance() == 1:
+                self.__rotate_right_left(node)
+            else: self.__rotate_left(node)
+
+        if parent != node:
+            self.__balance_tree_after_deletion(parent)
+
+    def __rotate_right(self, node):
+        son = node.left_son
+        node.left_son = son.right_son
+        if son.right_son:
+            son.right_son.parent = node
+        if self.root == node:
+            son.parent = son
+            self.root = son
+        else:
+            son.parent = node.parent
+            if node.is_right_child():
+                node.parent.right_son = son
+            else: node.parent.left_son = son
+        son.right_son = node
+        node.parent = son
+        node.update_height()
+        son.update_height()
+
+    def __rotate_left(self, node):
+        son = node.right_son
+        node.right_son = son.left_son
+        if son.left_son:
+            son.left_son.parent = node
+        if self.root == node:
+            son.parent = son
+            self.root = son
+        else:
+            son.parent = node.parent
+            if node.is_left_child():
+                node.parent.left_son = son
+            else: node.parent.right_son = son
+        son.left_son = node
+        node.parent = son
+        node.update_height()
+        son.update_height()
+
+    def __rotate_left_right(self, node):
+        son = node.left_son
+        grandson = son.right_son
+        son.right_son = grandson.left_son
+        if grandson.left_son:
+            grandson.left_son.parent = son
+        node.left_son = grandson.right_son
+        if grandson.right_son:
+            grandson.right_son.parent = node
+
+        son.update_height()
+        node.update_height()
+
+        grandson.left_son = son
+        son.parent = grandson
+        if self.root == node:
+            grandson.parent = grandson
+            self.root = grandson
+        else:
+            grandson.parent = node.parent
+            if node.is_left_child():
+                node.parent.left_son = grandson
+            else: node.parent.right_son = grandson
+        grandson.right_son = node
+        node.parent = grandson
+        grandson.update_height()
+
+    def __rotate_right_left(self, node):
+        son = node.right_son
+        grandson = son.left_son
+        son.left_son = grandson.right_son
+        if grandson.right_son:
+            grandson.right_son.parent = son
+        node.right_son = grandson.left_son
+        if grandson.left_son:
+            grandson.left_son.parent = node
+
+        son.update_height()
+        node.update_height()
+
+        grandson.right_son = son
+        son.parent = grandson
+        if self.root == node:
+            grandson.parent = grandson
+            self.root = grandson
+        else:
+            grandson.parent = node.parent
+            if node.is_right_child():
+                node.parent.right_son = grandson
+            else: node.parent.left_son = grandson
+        grandson.left_son = node
+        node.parent = grandson
+        grandson.update_height()
+
+
+    def print_tree(self):
+        print
+        self.__print_tree_rec(0, self.root)
+        print
+
+    def __print_tree_rec(self, tabs, node):
+        for i in range(0, tabs):
+            print " ",
+        if not node is None:
+            print str(node.value) + '(' + str(node.parent.value) + '/' + str(node.get_balance()) + ')'
+            self.__print_tree_rec(tabs + 3, node.right_son)
+            self.__print_tree_rec(tabs + 3, node.left_son)
+        else: print 'Nil'
+
+
+######################################################################
+# Simple tree structure test
+
+def node_comparison_function_test(self, node):
+    if self.value < node.value: return -1
+    elif self.value > node.value: return 1
+    else: return 0
+
+
+def test_avl_tree():
+    tree = BalancedBinarySearchTree(node_comparison_function_test)
+    while True:
+        v = raw_input()
+        if tree.contains(v):
+            tree.remove(v)
+        else: tree.insert(v)
+        tree.print_tree()
+######################################################################
---
+++ scanline.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+
+import sys
+from bbstree import BalancedBinarySearchTree
+
+#FIXME Is there another way?
+class Precision:
+    epsilon = 1e-100
+
+    @staticmethod
+    def is_zero(value):
+        return abs(value) <= Precision.epsilon
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
+
+    def __repr__(self):
+        return repr((self.x, self.y))
+
+    def set_left_extreme(self, extreme):
+        self.is_left_extreme = extreme
+
+    def set_segment_index(self, index):
+        self.index = index
+
+    def equals(self, point):
+        return Precision.is_zero(self.x - point.x) and \
+                Precision.is_zero(self.y - point.y)
+
+    def relative_quadrant(self, origin):
+        p = self.__translate(origin)
+        if p.x >= 0 and p.y >= 0: return 1
+        elif p.x <= 0 and p.y >= 0: return 2
+        elif p.x <= 0 and p.y <= 0: return 3
+        else: return 4
+
+    def area(self, point1, point2):
+        return (point1.x - self.x) * (point2.y - self.y) - \
+               (point1.y - self.y) * (point2.x - self.x)
+
+    def colinear(self, point1, point2):
+        return Precision.is_zero(self.area(point1, point2))
+
+    def on_the_left_side(self, left_extreme, right_extreme):
+        return self.area(left_extreme, right_extreme) > 0
+
+    def strictly_in_line_segment(self, point1, point2):
+        if not self.colinear(point1, point2): return False
+
+        if point1.x != point2.x:
+            return (point1.x < self.x < point2.x) or \
+                   (point2.x < self.x < point1.x)
+        else:
+            return (point1.y < self.y < point2.y) or \
+                   (point2.y < self.y < point1.y)
+
+    def __translate(self, origin):
+        return Point(self.x - origin.x, self.y - origin.y)
+
+
+class LineSegment:
+    origin = None
+
+    def __init__(self, p, q):
+        if LineSegment.origin is None:
+            raise Exception('Must call static method set_origin first')
+
+        if LineSegment.origin.on_the_left_side(p, q):
+            self.__set_segment(p, q)
+        else: self.__set_segment(q, p)
+        self.index = p.index
+
+    def __repr__(self):
+        return '[' + repr(self.left_extreme) + '; ' + repr(self.right_extreme) + ']'
+
+    def __set_segment(self, left_extreme, right_extreme):
+        left_extreme.set_left_extreme(True)
+        self.left_extreme = left_extreme
+        right_extreme.set_left_extreme(False)
+        self.right_extreme = right_extreme
+
+    @staticmethod
+    def set_origin(origin):
+        LineSegment.origin = origin
+
+
+class LineSegmentBalancedTree(BalancedBinarySearchTree):
+    # FIXME colinear points
+    @staticmethod
+    def points_comparison_function(self, node):
+        p = self.value.left_extreme
+        q = self.value.right_extreme
+        r = node.value.left_extreme
+        s = node.value.right_extreme
+        if p.equals(r) and q.equals(s):
+            return 0
+        elif p.on_the_left_side(r, s):
+            # self < node
+            return -1
+        else: return 1
+
+    def __init__(self):
+        BalancedBinarySearchTree.__init__(self, LineSegmentBalancedTree.points_comparison_function)
+
+
+
+def compare_polar_angles(point1, point2):
+    origin = LineSegment.origin
+    point1_quadrant = point1.relative_quadrant(origin)
+    point2_quadrant = point2.relative_quadrant(origin)
+    if point1_quadrant != point2_quadrant:
+        if point1_quadrant < point2_quadrant: return -1
+        else: return 1
+    else:
+        if point1.strictly_in_line_segment(origin, point2):
+            if not point1.is_left_extreme:
+                return 1
+            else: return -1
+        elif point2.strictly_in_line_segment(origin, point1):
+            if not point2.is_left_extreme:
+                return -1
+            else: return 1
+        else:
+            if point1.on_the_left_side(origin, point2): return 1
+            else: return -1
+
+
+def build_initial_tree(segments, sorted_extremes):
+    is_initial_segment = [True for each_element in segments]
+    for extreme in sorted_extremes:
+        if extreme.is_left_extreme:
+            is_initial_segment[extreme.index] = not is_initial_segment[extreme.index]
+        else: is_initial_segment[extreme.index] = False
+    initial_segments = [ segments[index] for index, present in
+                        enumerate(is_initial_segment) if present  ]
+
+    tree = LineSegmentBalancedTree()
+    for segment in initial_segments: tree.insert(segment)
+    return tree
+
+
+# Scan line algorithm
+def scan_line_radar(segments, extremes):
+    sorted_extremes = sorted(extremes, cmp=compare_polar_angles)
+    scan_line_segments = build_initial_tree(segments, sorted_extremes)
+
+    is_visible_segment = [False for each_element in segments]
+    visible = LineSegmentBalancedTree.min(scan_line_segments.root).value
+    is_visible_segment[visible.index] = True
+
+    for point in sorted_extremes:
+        if point.is_left_extreme:
+            scan_line_segments.insert(segments[point.index])
+        else:
+            scan_line_segments.remove(segments[point.index])
+        min_node = LineSegmentBalancedTree.min(scan_line_segments.root)
+        if not (min_node is None):
+            is_visible_segment[min_node.value.index] = True
+
+    return [ segments[index] for index, present in
+            enumerate(is_visible_segment) if present ]
+
+
+
+# Main program
+entry = sys.stdin.read().split()
+if len(entry) % 4 != 2:
+    raise Exception("Invalid input")
+
+# Reading input
+guard = Point(entry.pop(0), entry.pop(0))
+LineSegment.set_origin(guard)
+extremes = []
+segments = []
+while len(entry) > 0:
+    x = entry.pop(0)
+    y = entry.pop(0)
+    p = Point(x, y)
+
+    x = entry.pop(0)
+    y = entry.pop(0)
+    q = Point(x, y)
+
+    k = len(segments)
+    p.set_segment_index(k)
+    q.set_segment_index(k)
+    segments.append(LineSegment(p, q))
+    extremes.append(p)
+    extremes.append(q)
+
+
+guarded_walls = scan_line_radar(segments, extremes)
+print 'Visible segments from',
+print guard
+for wall in guarded_walls:
+    print wall
