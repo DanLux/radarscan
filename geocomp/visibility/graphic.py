178f43c6bdd87201763e888dd53114e713e33c54
Daniel Alves <danpaulalves@gmail.com>
2014-11-30 17:01:18 +0100

    Improved graphic interface animation.

--- geocomp/visibility/graphic.py
+++ geocomp/visibility/graphic.py
@@ -8,33 +8,40 @@ class GraphicAnimation:
         self.origin = origin
         self.points = points_list
 
-    def update(self):
+    def __update(self):
         control.thaw_update()
         control.update()
         control.freeze_update()
         control.sleep()
 
-    def draw_line(self, point1, point2, color):
+    def __draw_line(self, point1, point2, color):
         point1.lineto(point2, color)
-        self.update()
+        self.__update()
 
-    def erase_line(self, point1, point2):
+    def __erase_line(self, point1, point2):
         point1.remove_lineto(point2)
-        self.update()
+        self.__update()
 
-    def highlight(self, point):
-        point.hilight()
-        self.update()
+    def __highlight(self, point, color = config.COLOR_HI_POINT):
+        point.hilight(color)
+
+    def __unhighlight(self, point):
+        point.unhilight()
 
     def draw_animation(self):
+        self.__highlight(self.origin, config.COLOR_POINT)
+        self.__update()
         for k in range(0, len(self.points), 2):
-            self.draw_line(self.origin, self.points[k], config.COLOR_LINE_SPECIAL)
-            self.highlight(self.points[k])
-            self.erase_line(self.origin, self.points[k])
-            self.draw_line(self.origin, self.points[k + 1], config.COLOR_LINE_SPECIAL)
-            self.highlight(self.points[k + 1])
             point = self.points[k]
             other_point = self.points[k + 1]
-            self.draw_line(point, other_point, config.COLOR_PRIM)
-            self.erase_line(self.origin, self.points[k + 1])
-        self.erase_line(self.origin, self.points[len(self.points) - 1])
+            self.__draw_line(self.origin, point, config.COLOR_LINE_SPECIAL)
+            self.__highlight(point)
+            self.__erase_line(self.origin, point)
+            self.__draw_line(self.origin, other_point, config.COLOR_LINE_SPECIAL)
+            self.__highlight(other_point)
+            self.__draw_line(point, other_point, config.COLOR_PRIM)
+            self.__erase_line(self.origin, other_point)
+            self.__unhighlight(point)
+            self.__unhighlight(other_point)
+        self.__update()
+        self.__unhighlight(self.origin)
